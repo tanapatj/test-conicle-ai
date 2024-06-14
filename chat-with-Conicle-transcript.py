a01d5f78e3ee48cad3798fa76db7ce741109c96d
Tanapat Jiamsawas <tanapat.j@Conicle.com>
2024-06-14 12:09:38 +0700

    Test

--- chat-with-Conicle-transcript.py
+++ chat-with-Conicle-transcript.py
@@ -255,7 +255,7 @@ def main():
         mode = st.radio(
             "Select AI Mode",
             options=["Coach", "Personalized Learning Advisor"],
-            index=-1 if st.session_state['mode'] is None else ["Coach", "Personalized Learning Advisor"].index(
+            index=0 if st.session_state['mode'] is None else ["Coach", "Personalized Learning Advisor"].index(
                 st.session_state['mode'])
         )
         st.session_state['mode'] = mode
@@ -350,6 +350,7 @@ def main():
             """,
         unsafe_allow_html=True
     )
+    st.write("อยากสอนมากๆค่ะ")
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
     # Chat input
